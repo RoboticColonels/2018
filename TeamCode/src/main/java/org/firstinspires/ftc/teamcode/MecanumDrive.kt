439e35dfefc48aedd3b171fbfd59fa1a949b4e7b
FWP Robotics <robotheosis@gmail.com>
2018-11-10 09:53:44 -0600

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
@@ -3,35 +3,32 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver
-import com.qualcomm.robotcore.hardware.*
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector
 
 internal class MecanumDrive(hardwareMap: HardwareMap) {
     private val frontLeft: DcMotor = hardwareMap.get(DcMotor::class.java, "front_left")
     private val frontRight: DcMotor = hardwareMap.get(DcMotor::class.java, "front_right")
     private val rearLeft: DcMotor = hardwareMap.get(DcMotor::class.java, "rear_left")
-    private val rearRight: DcMotor = hardwareMap.get(DcMotor::class.java, "rear_left")
+    private val rearRight: DcMotor = hardwareMap.get(DcMotor::class.java, "rear_right")
     private val lift: DcMotor = hardwareMap.get(DcMotor::class.java, "lift")
     private val latch: Servo = hardwareMap.get(Servo::class.java, "latche_servo")
     private val dump: Servo = hardwareMap.get(Servo::class.java, "dump_servo")
-    // private val blinkinLedDriver: RevBlinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver::class.java, "blinkin")
 
     init {
         frontRight.direction = DcMotorSimple.Direction.REVERSE
         rearRight.direction = DcMotorSimple.Direction.REVERSE
-
-        // blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_RAINBOW_PALETTE)
     }
 
-    fun drive(x: Double, y: Double, z: Double) {
-        frontLeft.power = y + z + x
-        frontRight.power = y - z - x
-        rearLeft.power = y + z - x
-        rearRight.power = y - z + x
+    fun drive(x: Double, y: Double, r: Double) {
+        frontLeft.power = x + y + r
+        frontRight.power = x - y - r
+        rearLeft.power = x - y + r
+        rearRight.power = x + y - r
     }
 
     fun setLift(power: Double) {
@@ -43,7 +40,7 @@ internal class MecanumDrive(hardwareMap: HardwareMap) {
     }
 
     fun setDump(position: Double) {
-        latch.position = position
+        dump.position = position
     }
 }
 
@@ -54,43 +51,32 @@ internal class MecanumTeleOp : LinearOpMode() {
 
         waitForStart()
 
+        var latchPosition = 0.0
+
         while (opModeIsActive()) {
-            val x = (-gamepad1.left_stick_x).toDouble()
-            val y = gamepad1.left_stick_y.toDouble()
-            val z = (-gamepad1.right_stick_x).toDouble()
-            driver.drive(x, y, z)
+            val x = -gamepad1.left_stick_y.toDouble()
+            val y = gamepad1.left_stick_x.toDouble()
+            val r = -gamepad1.right_stick_x.toDouble()
+            driver.drive(x, y, r)
+
             driver.setLift(gamepad2.left_stick_y.toDouble())
+
+            if (gamepad2.right_bumper && latchPosition < 1.0) {
+                latchPosition += 0.05
+            } else if (gamepad2.left_bumper && latchPosition > -1.0) {
+                latchPosition -= 0.05
+            }
+            driver.setLatch(latchPosition)
         }
     }
 }
 
 @Autonomous(name = "Mecanum Autonomous", group = "Autonomous")
 internal class MecanumAutonomus : LinearOpMode() {
-    companion object {
-        private const val TFOD_MODEL_ASSET = "RoverRuckus.tflite"
-        private const val LABEL_GOLD_MINERAL = "Gold Mineral"
-        private const val LABEL_SILVER_MINERAL = "Silver Mineral"
-        private const val VUFORIA_KEY = "AWalSLT/////AAABme4BfTt4y0Wnrn7kzOornkwjbHAMMZnWYYe0WOYGLbmQ914wZ8gozbWoSgsLHUQ+asiEx9VUOuDMkr4LsB5hB3iyn/JZM7NB1fG15dZSIXVDXevQ2iy+6zaVS8q0JZ2dpvugMWHEpsrmadtlA13znd7nTwhvVoc+2gGc2hNb5k6G4qe4l3jusYi3KGXTkzLNvjjYuQGP8xsBkGkXY3oHt6LNwdBy9EAcxRYmKEQUoGOOSNAL5vqB2ZHN2pKrlxXJs0BMbyhm1U4PTPYriGCb2rBmgiBvcJmfz9T/vVLCpL3T9mV1GByU38J1aMzj6QrQKWD6/ImZYMchpb2yG5Xq0vb1WyFGR9IKp6eaAJ0XQL+q"
-    }
-
-    private var tfod: TFObjectDetector? = null
-    private val driver = MecanumDrive(hardwareMap)
-    private val gyro = hardwareMap.get(GyroSensor::class.java, "gyro")
-
     override fun runOpMode() {
-        run {
-            val parameters = VuforiaLocalizer.Parameters()
-            parameters.vuforiaLicenseKey = VUFORIA_KEY
-            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK
-            val vuforia = ClassFactory.getInstance().createVuforia(parameters)
-            val tfodMonitorViewId = hardwareMap.appContext.resources.getIdentifier(
-                    "tfodMonitorViewId", "id", hardwareMap.appContext.packageName)
-            val tfodParameters = TFObjectDetector.Parameters(tfodMonitorViewId)
-            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia)
-            tfod!!.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL)
-        }
+        val driver = MecanumDrive(hardwareMap)
 
-        runRecognition()
+        // runRecognition()
         telemetry.update()
 
         waitForStart()
@@ -103,7 +89,9 @@ internal class MecanumAutonomus : LinearOpMode() {
 
         driver.setLatch(0.0)
 
-        driver.drive(0.0, 0.5, 0.0)
+        sleep(4000)
+
+        driver.drive(-0.5, 0.0, 0.0)
 
         sleep(2000)
 
@@ -118,38 +106,5 @@ internal class MecanumAutonomus : LinearOpMode() {
         driver.setLift(0.0)
 
         driver.setLatch(0.0)
-
-        tfod?.shutdown()
-    }
-
-    private fun runRecognition(): String? {
-        val updatedRecognitions = tfod?.updatedRecognitions
-        if (updatedRecognitions != null) {
-            telemetry.addData("# Object Detected", updatedRecognitions.size)
-            if (updatedRecognitions.size == 3) {
-                var goldMineralX = -1
-                var silverMineral1X = -1
-                var silverMineral2X = -1
-                for (recognition in updatedRecognitions) {
-                    if (recognition.label == LABEL_GOLD_MINERAL) {
-                        goldMineralX = recognition.left.toInt()
-                    } else if (silverMineral1X == -1) {
-                        silverMineral1X = recognition.left.toInt()
-                    } else {
-                        silverMineral2X = recognition.left.toInt()
-                    }
-                }
-                if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                    if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
-                        return "left"
-                    }
-                    if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
-                        return "right"
-                    }
-                    return "center"
-                }
-            }
-        }
-        return null
     }
 }
