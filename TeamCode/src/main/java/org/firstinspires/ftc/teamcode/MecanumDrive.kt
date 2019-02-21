dfad614b26c646dda42686fc94d09542ccd31c78
Robotheosis <robotheosis@gmail.com>
2019-02-21 13:44:15 -0600

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
@@ -2,99 +2,93 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.robotcore.external.ClassFactory
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector
 
 internal class MecanumDrive(hardwareMap: HardwareMap) {
+    companion object {
+        const val COUNTS_PER_ROTATION: Int = 657
+    }
     private val frontLeft: DcMotor = hardwareMap.get(DcMotor::class.java, "front_left")
     private val frontRight: DcMotor = hardwareMap.get(DcMotor::class.java, "front_right")
     private val rearLeft: DcMotor = hardwareMap.get(DcMotor::class.java, "rear_left")
     private val rearRight: DcMotor = hardwareMap.get(DcMotor::class.java, "rear_right")
+    private val arm: DcMotor = hardwareMap.get(DcMotor::class.java, "arm")
     private val lift: DcMotor = hardwareMap.get(DcMotor::class.java, "lift")
-    private val latch: Servo = hardwareMap.get(Servo::class.java, "latche_servo")
-    private val leftCol: DcMotor = hardwareMap.get(DcMotor::class.java, "collector_left")
-    private val rightCol: DcMotor = hardwareMap.get(DcMotor::class.java, "collector_right")
-    private val collector: DcMotor = hardwareMap.get(DcMotor::class.java, "collector")
+    private val dump: DcMotor = hardwareMap.get(DcMotor::class.java, "dump")
+    private val collector: DcMotor = hardwareMap.get(DcMotor::class.java, "collect")
+    private val marker: Servo = hardwareMap.get(Servo::class.java, "marker")
 
     init {
         frontRight.direction = DcMotorSimple.Direction.REVERSE
         rearRight.direction = DcMotorSimple.Direction.REVERSE
 
-        rightCol.direction = DcMotorSimple.Direction.REVERSE
+        lift.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        arm.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+        arm.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
     }
 
-    fun drive(x: Double, y: Double, r: Double) {
+    fun drive(distance: Double, left: Double, right: Double) {
+        frontLeft.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        rearLeft.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        frontRight.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        rearRight.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+
+        frontLeft.mode = DcMotor.RunMode.RUN_TO_POSITION
+        rearLeft.mode = DcMotor.RunMode.RUN_TO_POSITION
+        frontRight.mode = DcMotor.RunMode.RUN_TO_POSITION
+        rearLeft.mode = DcMotor.RunMode.RUN_TO_POSITION
+
+        frontLeft.power = left
+        rearLeft.power = left
+        frontRight.power = right
+        rearRight.power = right
+
+        val leftDist = (if (left < 0.0) -distance else distance) * COUNTS_PER_ROTATION
+        val rightDist = (if (right < 0.0) -distance else distance) * COUNTS_PER_ROTATION
+
+        frontLeft.targetPosition = leftDist.toInt()
+        rearLeft.targetPosition = leftDist.toInt()
+        frontRight.targetPosition = rightDist.toInt()
+        rearRight.targetPosition = rightDist.toInt()
+    }
+
+    fun driveMec(x: Double, y: Double, r: Double) {
         frontLeft.power = x + y + r
         frontRight.power = x - y - r
         rearLeft.power = x - y + r
         rearRight.power = x + y - r
     }
 
-    fun setLift(power: Double) {
-        lift.power = power
+
+    fun setArmPower(pow: Double) {
+        arm.power = pow
     }
 
-    fun setLatch(position: Double) {
-        latch.position = position
+    fun setDumpPower(pow: Double) {
+        dump.power = pow
     }
 
-    fun moveCollector(power: Double) {
-        collector.power = power
+    fun setLiftPower(pow: Double) {
+        lift.power = pow
     }
 
-    fun setCollectorSpin(power: Double) {
-        leftCol.power = power
-        rightCol.power = power
+    fun setCollectPower(pow: Double) {
+        collector.power = pow
     }
-}
 
-@TeleOp(name = "Mecanum TeleOp", group = "TeleOp")
-internal class MecanumTeleOp : LinearOpMode() {
-    override fun runOpMode() {
-        val driver = MecanumDrive(hardwareMap)
-
-        driver.setLatch(0.0)
-
-        waitForStart()
-
-        while (opModeIsActive()) {
-            val x = -gamepad1.left_stick_y.toDouble()
-            val y = gamepad1.left_stick_x.toDouble()
-            val r = gamepad1.right_stick_x.toDouble()
-            driver.drive(x, y, r)
-
-            driver.setLift((gamepad2.right_trigger - gamepad2.left_trigger).toDouble())
-
-            if (gamepad2.right_bumper) {
-                driver.setLatch(1.0)
-            } else if (gamepad2.left_bumper) {
-                driver.setLatch(-1.0)
-            } else {
-                driver.setLatch(0.0)
-            }
-
-            driver.moveCollector(-gamepad2.left_stick_y.toDouble())
-            if (gamepad2.a) {
-                driver.setCollectorSpin(1.0)
-            } else if (gamepad2.b) {
-                driver.setCollectorSpin(-1.0)
-            } else {
-                driver.setCollectorSpin(0.0)
-            }
-
-            idle()
-        }
+    fun setMarkerPosition(position: Double) {
+        marker.position = position
     }
 }
 
-@Autonomous(name = "Auto Hang", group = "Autonomous")
+@Autonomous(name = "AutoCraterNative", group = "Autonomous")
 internal class MecanumAutonomusHang : LinearOpMode() {
     companion object {
         private const val TFOD_MODEL_ASSET = "RoverRuckus.tflite"
@@ -103,6 +97,7 @@ internal class MecanumAutonomusHang : LinearOpMode() {
         private const val VUFORIA_KEY = "AWalSLT/////AAABme4BfTt4y0Wnrn7kzOornkwjbHAMMZnWYYe0WOYGLbmQ914wZ8gozbWoSgsLHUQ+asiEx9VUOuDMkr4LsB5hB3iyn/JZM7NB1fG15dZSIXVDXevQ2iy+6zaVS8q0JZ2dpvugMWHEpsrmadtlA13znd7nTwhvVoc+2gGc2hNb5k6G4qe4l3jusYi3KGXTkzLNvjjYuQGP8xsBkGkXY3oHt6LNwdBy9EAcxRYmKEQUoGOOSNAL5vqB2ZHN2pKrlxXJs0BMbyhm1U4PTPYriGCb2rBmgiBvcJmfz9T/vVLCpL3T9mV1GByU38J1aMzj6QrQKWD6/ImZYMchpb2yG5Xq0vb1WyFGR9IKp6eaAJ0XQL+q"
     }
 
+    private var lastDetection: String? = null
     private var tfod: TFObjectDetector? = null
 
     override fun runOpMode() {
@@ -111,121 +106,141 @@ internal class MecanumAutonomusHang : LinearOpMode() {
         run {
             val parameters = VuforiaLocalizer.Parameters()
             parameters.vuforiaLicenseKey = VUFORIA_KEY
-            // parameters.cameraName = hardwareMap.get(WebcamName::class.java, "camera")
-            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT
+            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK
             val vuforia = ClassFactory.getInstance().createVuforia(parameters)
             val tfodMonitorViewId = hardwareMap.appContext.resources.getIdentifier(
                     "tfodMonitorViewId", "id", hardwareMap.appContext.packageName)
             val tfodParameters = TFObjectDetector.Parameters(tfodMonitorViewId)
+            tfodParameters.minimumConfidence = 0.30
             tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia)
             tfod!!.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL)
+            tfod!!.activate()
         }
 
-        runRecognition()
-
-        waitForStart()
-
-        driver.setLift(-0.5)
-        sleep(1500)
-        driver.setLift(0.0)
-
-        driver.moveCollector(0.5)
-        sleep(2000)
-        driver.moveCollector(0.0)
-
-        when (runRecognition()) {
-            "left" -> {
-                driver.drive(0.5, -0.5, 0.0)
-                sleep(1500)
-                driver.drive(0.0, 0.5, 0.0)
-                sleep(1500)
-                driver.drive(0.0, 0.0, 0.0)
-            }
-            "right" -> {
-                driver.drive(0.5, 0.5, 0.0)
-                sleep(1500)
-                driver.drive(0.0, -0.0, 0.0)
-                sleep(1500)
-                driver.drive(0.0, 0.0, 0.0)
-            }
-            "center" -> {
-                driver.drive(0.5, 0.0, 0.0)
-                sleep(1500)
-                driver.drive(0.0, 0.0, 0.0)
-            }
-            null -> {
-                driver.drive(0.5, 0.0, 0.0)
-                sleep(1500)
-                driver.drive(0.0, 0.0, 0.0)
-            }
+        while (!opModeIsActive()) {
+            runRecognition()
         }
-
-        driver.drive(0.5, 0.0, 0.0)
-        sleep(1500)
-        driver.drive(0.0, 0.0, 0.0)
-
-        driver.setCollectorSpin(0.5)
-        sleep(1500)
-        driver.setCollectorSpin(0.0)
-
-        driver.moveCollector(-1.0)
-        sleep(1500)
-        driver.moveCollector(0.0)
-
-        driver.drive(0.0, 0.0, 0.5)
-        sleep(800)
-        driver.drive(0.0, 0.0, 0.0)
-
-        driver.drive(0.5, 0.0, 0.0)
-        sleep(5000)
-        driver.drive(0.0, 0.0, 0.0)
-
+        val mineralPosition = runRecognition()
+        tfod!!.deactivate()
         tfod!!.shutdown()
+
+        // lower down
+        driver.setLiftPower(-1.0)
+        sleep(1100)
+        driver.setLiftPower(0.0)
+
+        // unlatch
+        driver.driveMec(0.0, 0.5, 0.0)
+        sleep(200)
+        driver.driveMec(0.0, 0.0, 0.0)
+
+        driver.setArmPower(1.0)
+
+//        // sample
+//        when (mineralPosition) {
+//            "left", null -> {
+//                driver.drive(0.3, 0.5, 0.5)
+//            }
+//            "right" -> {
+//                driver.drive(0.25, 0.5, 0.5)
+//            }
+//            "center" -> {
+//                driver.drive(0.65, 0.5, 0.5)
+//            }
+//        }
+//
+//        driver.setArmPower(0.0)
+//
+//        driver.drive(0.31, 0.5, 0.5)
+//        driver.drive(1.43, 0.5, -0.5)
+
+//        val depotDrivePower = 0.7
+//        when (mineralPosition) {
+//            "left", null -> {
+//                driver.drive(3.8, -depotDrivePower, -depotDrivePower)
+//            }
+//            "right" -> {
+//                driver.drive(3.5, -depotDrivePower, -depotDrivePower)
+//            }
+//            "center" -> {
+//                driver.drive(3.3, -depotDrivePower, -depotDrivePower)
+//            }
+//        }
+//        sampleForwardAndBack(driver)
+
+//        driver.drive(0.7, -0.5, 0.5)
+//        driver.drive(1.8, 0.5, -0.5)
+//
+//        when (mineralPosition) {
+//            "left", null -> {
+//                driver.drive(3.8, -depotDrivePower, -depotDrivePower)
+//            }
+//            "right" -> {
+//                driver.drive(3.0, -depotDrivePower, -depotDrivePower)
+//            }
+//            "center" -> {
+//                driver.drive(3.2, -depotDrivePower, -depotDrivePower)
+//            }
+//        }
+//
+//        sleep(200)
+//
+//        driver.setMarkerPosition(-1.0)
+//        sleep(2000)
+//        driver.setMarkerPosition(0.5)
+//
+//        sleep(1000)
+//
+//        driver.drive(0.3, 0.5, -0.5)
+//        driver.drive(4.75, 1.0, 1.0)
+//
+//        driver.setArmPower(0.5)
+//        sleep(400)
+//        driver.setArmPower(0.0)
     }
 
-    private fun runRecognition(): String? {
+    private fun sampleForwardAndBack(driver: MecanumDrive) {
+        driver.drive(0.69, 0.3, 0.3)
+        driver.setCollectPower(1.0)
+        sleep(1500)
+        driver.setCollectPower(0.0)
+        sleep(600)
+    }
+
+    private fun eagerRunRecognition(): String? {
         val updatedRecognitions = tfod?.updatedRecognitions
         if (updatedRecognitions != null) {
             telemetry.addData("# Object Detected", updatedRecognitions.size)
-            if (updatedRecognitions.size == 3) {
-                var goldMineralX = -1
-                var silverMineral1X = -1
-                var silverMineral2X = -1
-                for (recognition in updatedRecognitions) {
-                    if (recognition.label == LABEL_GOLD_MINERAL) {
-                        goldMineralX = recognition.left.toInt()
-                    } else if (silverMineral1X == -1) {
-                        silverMineral1X = recognition.left.toInt()
-                    } else {
-                        silverMineral2X = recognition.left.toInt()
-                    }
-                }
-                if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                    if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
-                        return "left"
-                    }
-                    if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
-                        return "right"
-                    }
-                    return "center"
+            var goldMineralX: Int? = null
+            for (recognition in updatedRecognitions) {
+                if (recognition.label == LABEL_GOLD_MINERAL) {
+                    goldMineralX = recognition.right.toInt()
                 }
             }
+            if (goldMineralX == null) {
+                return "right"
+            }
+            if (goldMineralX > 400) {
+                return "center"
+            }
+            return "left"
         }
         return null
     }
-}
 
-@Autonomous(name = "Auto Crater", group = "Autonomous")
-internal class MecanumAutonomusCrater : LinearOpMode() {
-    override fun runOpMode() {
-        val driver = MecanumDrive(hardwareMap)
-
-        waitForStart()
-
-        driver.drive(-0.4, 0.0, 0.0)
-
-        sleep(4000)
-
-        driver.drive(0.0, 0.0, 0.0)
+    private fun runRecognition(): String? {
+        val current = eagerRunRecognition()
+        if (current != null) {
+            lastDetection = current
+        }
+        telemetry.addData("detection", lastDetection)
+        return lastDetection
+        /*
+        return lastDetection
+            return current
+        } else {
+            return last
+        }
+        */
     }
 }
